04df61ee34133a3749f0ed799673073776a56917
Balint Lakatos <lakatosbalint911@gmail.com>
2023-08-16 19:26:34 +0200

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Bálna on 2023. 08. 16..
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
